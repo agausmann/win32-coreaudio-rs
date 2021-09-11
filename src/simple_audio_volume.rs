92330a2f415b2830b4183a5872bab907b4239a23
Adam Gausmann <agausmann@fastmail.com>
2021-09-11 01:25:36 -0500

    Add SimpleAudioVolume

--- src/simple_audio_volume.rs
+++ src/simple_audio_volume.rs
@@ -1,5 +1,10 @@
-use crate::bindings::Windows::Win32::Media::Audio::CoreAudio::ISimpleAudioVolume;
+use windows::Guid;
 
+use crate::{
+    bindings::Windows::Win32::Media::Audio::CoreAudio::ISimpleAudioVolume, util::as_raw_or_null,
+};
+
+/// See also: [`ISimpleAudioVolume`](https://docs.microsoft.com/en-us/windows/win32/api/audioclient/nn-audioclient-isimpleaudiovolume)
 pub struct SimpleAudioVolume {
     inner: ISimpleAudioVolume,
 }
@@ -7,5 +12,32 @@ pub struct SimpleAudioVolume {
 impl SimpleAudioVolume {
     pub(crate) fn new(inner: ISimpleAudioVolume) -> Self {
         Self { inner }
+    }
+
+    /// See also: [`ISimpleAudioVolume::GetMasterVolume`](https://docs.microsoft.com/en-us/windows/win32/api/audioclient/nf-audioclient-isimpleaudiovolume-getmastervolume)
+    pub fn get_master_volume(&self) -> windows::Result<f32> {
+        unsafe { self.inner.GetMasterVolume() }
+    }
+
+    /// See also: [`ISimpleAudioVolume::GetMute`](https://docs.microsoft.com/en-us/windows/win32/api/audioclient/nf-audioclient-isimpleaudiovolume-getmute)
+    pub fn get_mute(&self) -> windows::Result<bool> {
+        unsafe { self.inner.GetMute().map(Into::into) }
+    }
+
+    /// See also: [`ISimpleAudioVolume::SetMasterVolume`](https://docs.microsoft.com/en-us/windows/win32/api/audioclient/nf-audioclient-isimpleaudiovolume-setmastervolume)
+    pub fn set_master_volume(
+        &self,
+        volume_level: f32,
+        event_context: Option<&Guid>,
+    ) -> windows::Result<()> {
+        unsafe {
+            self.inner
+                .SetMasterVolume(volume_level, as_raw_or_null(event_context))
+        }
+    }
+
+    /// See also: [`ISimpleAudioVolume::SetMute`](https://docs.microsoft.com/en-us/windows/win32/api/audioclient/nf-audioclient-isimpleaudiovolume-setmute)
+    pub fn set_mute(&self, mute: bool, event_context: Option<&Guid>) -> windows::Result<()> {
+        unsafe { self.inner.SetMute(mute, as_raw_or_null(event_context)) }
     }
 }
